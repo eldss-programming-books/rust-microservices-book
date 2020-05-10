28957c50afb0a8b3ad14e19529ddce853e59abca
Evan <EvanLDouglass@gmail.com>
2020-05-10 12:06:07 -0700

    Add method to create a channel

--- chat/src/lib.rs
+++ chat/src/lib.rs
@@ -37,6 +37,39 @@ impl Api {
             .get_result(&self.conn)
             .map_err(Error::from)
     }
+
+    /// Create a new channel
+    pub fn create_channel(
+        &self,
+        user_id: Id,
+        title: &str,
+        is_public: bool,
+    ) -> Result<Channel, Error> {
+        self.conn.transaction::<_, _, _>(|| {
+            // Create the channel
+            let channel: Channel = insert_into(channels::table)
+                .values((
+                    channels::user_id.eq(user_id),
+                    channels::title.eq(title),
+                    channels::is_public.eq(is_public),
+                ))
+                .returning((
+                    channels::id,
+                    channels::user_id,
+                    channels::title,
+                    channels::is_public,
+                    channels::created_at,
+                    channels::updated_at,
+                ))
+                .get_result(&self.conn)
+                .map_err(Error::from)?;
+
+            // Add the user who created it to the channel
+            self.add_member(channel.id, user_id)?;
+
+            Ok(channel)
+        })
+    }
 }
 
 #[cfg(test)]
